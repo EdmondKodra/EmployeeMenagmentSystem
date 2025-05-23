15fa5a236d89ee510ec6a5db6c141e5532558976
EdmondKodra <121366943+EdmondKodra@users.noreply.github.com>
2025-05-23 16:24:20 +0200

    Create db.js

---
+++ server/utils/db.js
@@ -0,0 +1,19 @@
+import mysql from 'mysql'
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "employeems",
+    port: "4306"
+})
+
+con.connect(function(err){
+    if(err){
+        console.log("Connection error:", err.message);
+    }
+    else{
+        console.log("Connected to MySQL");
+    }
+})
+export default con; 
